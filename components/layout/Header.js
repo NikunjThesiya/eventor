90d4fccf7c1df93e2a9c52dd764c58860c5d2c3c
thesiya-nikunj <wd.nikunj@gmail.com>
2021-06-22 11:16:42 +0530

    Front End Completed

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
 	const router = useRouter();
 	return (
 		<header className="z-50 w-full px-8 py-4 flex items-center justify-between bg-gradient-to-r from-purple-400 to-purple-600 sticky top-0 shadow-xl">
-			<div className="logo text-xl md:text-3xl text-white">
+			<div className="logo text-2xl md:text-3xl text-white">
 				<Link href="/">Eventor</Link>
 			</div>
 			<span onClick={() => router.push("/events")} className="btnWhite">
